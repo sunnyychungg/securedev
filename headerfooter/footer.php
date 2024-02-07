64654cc0b09d0077b31e1cb1b76174408c2ff793
sunnyychungg <157390829+sunnyychungg@users.noreply.github.com>
2024-02-07 19:47:37 +0000

    Folder and headers used

---
+++ headerfooter/footer.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang=en>
+	<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top text-light" style="background-color: black;">
+        <p class="col-md-4 mb-0 text-body-secondary">@ 2024 ChatEZSE, Inc</p>
+
+        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+          <!-- Company Image -->
+          <!-- <img height="30px" src="./logos/revice_logo_white.png" alt="revice logo" /> -->
+        </a>
+
+        <p class="col-md-4 d-flex align-items-center justify-content-end d-flex">
+        	All rights reserved.
+        <p>
+	</footer>
+</html>
---
+++ headerfooter/header.php
@@ -0,0 +1,149 @@
+<?php
+session_start();
+ob_start();
+
+include "db_connect.php";
+
+$login = 0;
+$invalid = 0;
+
+if (($_SERVER['REQUEST_METHOD'] == 'POST') && ($_POST['login'] == "go")) {
+
+	// Validates text, trims, etc.
+	function validate($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+
+	$username = validate($_POST['username']);
+	$password = validate($_POST['password']);
+
+	// Deals with empty boxes and correct info.
+	if (empty($username) || (empty($password)))  {
+		$invalid = 1;
+		exit();
+	}
+	else {
+		$sql = " SELECT * FROM users WHERE username='$username' AND password='$password' ";
+
+		$result = mysqli_query($conn, $sql);
+
+		if (mysqli_num_rows($result) === 1) {
+			session_start();
+			$_SESSION['username'] = $username;
+			$login = 1;
+			header('Location: profile.php');
+			exit();
+		}
+		else {
+			$invalid = 1;
+		}
+	}
+}
+
+?>
+<!DOCTYPE html>
+<html lang=en>
+	<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+				<link rel="stylesheet" href="./stylesheets/styles.css">
+
+				<title>ReVice</title>
+				 </head>
+
+			 <body>
+				<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark container-12">
+					<!-- NavBar Logo-->
+					<a class="navbar-brand px-4" href="homepage.php"><img width="180px" height="39px" src="logos/" alt="logo" /></a>
+
+					<!-- If there is a SESSION with a username -->
+					<div class="collapse navbar-collapse flex-row-reverse px-4 navbar-dark">
+						<ul class="navbar-nav">
+							<?php
+						if (isset($_SESSION['username'])) :
+								?>
+										<li class="nav-item dropdown px-2" >
+												<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+														<img width="30px" height="30px" src="./logos/userlogo.png" />
+														<span class="px-2 username-text"><?=$_SESSION['username'];?> </span>
+												</a>
+												<div class="dropdown-menu dropdown-menu-right bg-dark" aria-labelledby="navbarDropdownMenuLink">
+														<a class="dropdown-item text-light" href="profile.php">Profile</a>
+														<hr>
+														<a class="dropdown-item text-light" href="./logout.php">Logout</a>
+												</div>
+										</li>
+										<?php
+						else :
+								?>
+										<li class="nav-item px-2">
+												<a class="nav-link text-light" data-toggle="modal" data-target="#signinPage" href="">
+														<img width="30px" height="30px" src="./logos/userlogo.png" />
+												</a>
+										</li>
+										<?php
+						endif;
+						?>
+						</ul>
+					</div>
+				</nav>
+
+				<!-- Modal Box for Log In/Sign Up-->
+					<div class="modal fade text-dark" aria-hidden="true" tabindex="-1" id="signinPage">
+			<div class="modal-dialog">
+				<div class="modal-content">
+							 		<div class="modal-header text-center">
+							 			<h4 class="modal-title text-center font-weight-light d-inline-block w-100">Log In</h4>
+						<button type="button" class="close d-inline-block float-right" data-dismiss="modal" aria-label="Close">
+													<span aria-hidden="true">&times;</span>
+						</button>
+							 		</div>
+							 		<div class="modal-body ">
+							 			<form action='homepage.php' method='POST'>
+													<div class="form-group">
+														<label>Username:</label>
+														<input type="text" class="form-control" name="username" placeholder="Enter username">
+													</div>
+
+													<div class="form-group">
+														<label>Password:</label>
+														<input type="password" class="form-control" id="password" name="password" placeholder="Password">
+													</div>
+
+													<hr>
+													<button type="submit" class="btn btn-primary w-100" name="login" value="go">Log In</button>
+													<hr>
+
+													<!-- Sign Up Redirect -->
+													<h6 class="text-center font-weight-light">Need an account? <a href='signup.php'>SIGN UP</a></h6>
+											</form>
+							 		</div>
+				</div>
+						 </div>
+					 </div>
+
+									 <?php
+								if ($login) {
+										echo '<div class="alert alert-secondary" role="alert">
+	Successfully logged in!
+</div>';
+								}
+
+								if ($invalid) {
+										echo '<div class="alert alert-danger" role="alert">
+	Invalid Credentials!
+</div>';
+								}
+								?>
+
+				<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+		</body>
+</html>
