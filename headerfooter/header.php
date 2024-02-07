ffd6375dab3ef2dcbd44ba3aa3ec4f5fefa1fb9d
sunnyychungg <s.chung@2022.ljmu.ac.uk>
2024-02-07 19:51:22 +0000

    Fixed title

--- headerfooter/header.php
+++ headerfooter/header.php
@@ -54,7 +54,7 @@ if (($_SERVER['REQUEST_METHOD'] == 'POST') && ($_POST['login'] == "go")) {
 				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 				<link rel="stylesheet" href="./stylesheets/styles.css">
 
-				<title>ReVice</title>
+				<title>ChatEZSE</title>
 				 </head>
 
 			 <body>
