b7eb37f5cf907e44edc3e127aa06925946e9161f
owenlover911 <157390468+owenlover911@users.noreply.github.com>
2024-03-07 16:59:57 +0000

    upload.php

--- upload.php
+++ upload.php
@@ -31,52 +31,37 @@
 <?php
 include "headerfooter/header.php";
 
-if (isset($_POST['submit']) && isset ($_FILES['my_image'])) {
+if (isset($_POST['submit']) && isset($_FILES['my_image'])) {
 
-    echo "<pre>";
-    print_r($_FILES['my_image']);
-    echo "</pre>";
-
+    // Get information about the uploaded file
     $img_name = $_FILES['my_image']['name'];
     $img_size = $_FILES['my_image']['size'];
-    $tmp_name = $_FILES['my_image']['tmo_name'];
+    $tmp_name = $_FILES['my_image']['tmp_name'];
     $error = $_FILES['my_image']['error'];
 
+    // Check for upload errors
     if ($error === 0) {
-        if ($img_size > 125000) {
-            $em = "Sorry, your file is too large.";
-            header("Location: index.php?error=$em");            
-        }else {
-            $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
-            $img_ex_lc = strtolower($img_ex);
+        //directory of stored images
+        $upload_directory = 'uploads/';
 
-            $allowed_exs = array("jpg", "jpeg", "png");
+        //unique file name to avoid overwriting existing files
+        $new_image_name = uniqid('IMG-', true) . '_' . $img_name;
 
-            if (in_array($img_ex_lc, $allowed_exs)) {
-                $new_image_name = uniqid("IMG-", true).'.'.$img_ex_lc;
-                $img_upload_path = 'uploads/'.$new_img_name;
-                move_uploaded_file($tmp_name, $img_upload_path);
+        //full path to the upload directory
+        $img_upload_path = $upload_directory . $new_image_name;
 
-                // Insert into Database
-                $sql = "INSERT INTO images(image_url)
-                        VALUES('$new_image_name')";
-                        mysqli_query($conn, $sql);
-                        header("Location: view.php");
-            }else {
-                $em = "You can't upload files of this type";
-                header ("Location: index.php?error=$em");
-            }
-
+        // Move the uploaded file to the desired directory
+        if (move_uploaded_file($tmp_name, $img_upload_path)) {
+            echo "File uploaded successfully.";
+        } else {
+            echo "Error uploading file.";
         }
-    }else { 
-        $em = "uknown error occurred!";
-        header("Location: index.php?error=$em");
+    } else {
+        echo "Upload failed with error code: $error";
     }
-
-}else {
-    header("Location: index.php");
+} else {
+    echo "No file uploaded.";
 }
-
 ?>
 <?php
 include "headerfooter/footer.php";
